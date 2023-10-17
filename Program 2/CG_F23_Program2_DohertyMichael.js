9cdcf403269fd0563a99db1ecdbfdde67270f8c8
Mikecamdo <mcdoherty@smu.edu>
2023-10-17 13:38:09 -0500

    Moving Around Room
    Changed projection matrix to perspective and added functionality to move around the room. Currently lots of commented out code (which I wanted to commit so I can see what I tried before).

--- Program 2/CG_F23_Program2_DohertyMichael.js
+++ Program 2/CG_F23_Program2_DohertyMichael.js
@@ -85,11 +85,84 @@ window.onload = function init() {
 
     // ! this is how you change the coordinates that you're working with
     // ! currently set to 0-100 for X, Y, and Z axes
-    projectionMatrix = ortho(-10, 10, -10, 10, -10, 10);
+    //projectionMatrix = ortho(-10, 10, -10, 10, -10, 10);
+    // ! change first value to change how much you can see in the field of view
+    projectionMatrix = perspective(100, 1, 0.1, 50);
     console.log('PROJECTIOJ')
     console.log(projectionMatrix)
 
     gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),  false, flatten(projectionMatrix) );
+
+    var currentAngle = 0;
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "E" || event.key === 'e') {
+            let tester1 = vec4(cameraTarget[0], cameraTarget[1], cameraTarget[2], 1);
+            currentAngle -= 1;
+
+            let backToOrigin = translate(-cameraPosition[0], -cameraPosition[1], -cameraPosition[2]);
+            let backToOriginal = translate(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+
+            let finalMatrix = mult(backToOriginal, rotateY(-1));
+            finalMatrix = mult(finalMatrix, backToOrigin);
+
+            tester1 = mult(finalMatrix, tester1);
+            cameraTarget[0] = tester1[0];
+            cameraTarget[1] = tester1[1];
+            cameraTarget[2] = tester1[2];
+
+            // currentAngle -= 1;
+            // cameraTarget[0] = 10 * Math.sin(radians(currentAngle));
+            // cameraTarget[2] = 10 * Math.cos(radians(currentAngle));
+        } else if (event.key === "Q" || event.key === 'q') {
+            let tester2 = vec4(cameraTarget[0], cameraTarget[1], cameraTarget[2], 1);
+            currentAngle += 1;
+
+            let backToOrigin = translate(-cameraPosition[0], -cameraPosition[1], -cameraPosition[2]);
+            let backToOriginal = translate(cameraPosition[0], cameraPosition[1], cameraPosition[2]);
+
+            let finalMatrix = mult(backToOriginal, rotateY(1));
+            finalMatrix = mult(finalMatrix, backToOrigin);
+
+            tester2 = mult(finalMatrix, tester2);
+            cameraTarget[0] = tester2[0];
+            cameraTarget[1] = tester2[1];
+            cameraTarget[2] = tester2[2];
+
+            // currentAngle += 1;
+            // cameraTarget[0] = 10 * Math.sin(radians(currentAngle));
+            // cameraTarget[2] = 10 * Math.cos(radians(currentAngle));
+        } else if (event.key === "W" || event.key === 'w') {
+            // TODO cos(0) = 1, sin(1) = 0
+            // let tester3 = vec4(cameraPosition[0], cameraPosition[1], cameraPosition[2], 1);
+            // tester3 = mult(translate(Math.sin(radians(currentAngle)) * -0.1, 0, Math.cos(radians(currentAngle)) * -0.1), tester3);
+            // cameraPosition[0] = tester3[0];
+            // cameraPosition[1] = tester3[1];
+            // cameraPosition[2] = tester3[2];
+            
+            // ! to walk straight, apply same values to both camera and target
+            cameraTarget[0] += 0.1 * Math.sin(radians(currentAngle));
+            cameraPosition[0] += 0.1 * Math.sin(radians(currentAngle));
+
+            cameraTarget[2] += 0.1 * Math.cos(radians(currentAngle));
+            cameraPosition[2] += 0.1 * Math.cos(radians(currentAngle));
+        } else if (event.key === "S" || event.key === 's') {
+            // let tester3 = vec4(cameraPosition[0], cameraPosition[1], cameraPosition[2], 1);
+            // tester3 = mult(translate(Math.sin(radians(currentAngle)) * 0.1, 0, Math.cos(radians(currentAngle)) * 0.1), tester3);
+            // cameraPosition[0] = tester3[0];
+            // cameraPosition[1] = tester3[1];
+            // cameraPosition[2] = tester3[2];
+            cameraTarget[0] -= 0.1 * Math.sin(radians(currentAngle));
+            cameraPosition[0] -= 0.1 * Math.sin(radians(currentAngle));
+
+            cameraTarget[2] -= 0.1 * Math.cos(radians(currentAngle));
+            cameraPosition[2] -= 0.1 * Math.cos(radians(currentAngle));
+        }
+        // console.log('Camera Position:');
+        // console.log(cameraPosition);
+        console.log('Camera Target:');
+        console.log(cameraTarget);
+    });
 
     render();
 }
@@ -102,14 +175,14 @@ function getVertices() {
     let vertices = [];
 
     vertices.push( // vertices for walls of room
-        vec4(-10.0, -10.0, -10.0, 1.0), // 0
-        vec4(-10.0,  10.0, -10.0, 1.0), // 1
-        vec4( 10.0,  10.0, -10.0, 1.0), // 2
-        vec4( 10.0, -10.0, -10.0, 1.0), // 3
-        vec4(-10.0, -10.0,  10.0, 1.0), // 4
-        vec4(-10.0,  10.0,  10.0, 1.0), // 5
-        vec4( 10.0,  10.0,  10.0, 1.0), // 6
-        vec4( 10.0, -10.0,  10.0, 1.0)  // 7
+        vec4(-10.0, -10.0,  10.0, 1.0), // 0
+        vec4(-10.0,  10.0,  10.0, 1.0), // 1
+        vec4( 10.0,  10.0,  10.0, 1.0), // 2
+        vec4( 10.0, -10.0,  10.0, 1.0), // 3
+        vec4(-10.0, -10.0, -10.0, 1.0), // 4
+        vec4(-10.0,  10.0, -10.0, 1.0), // 5
+        vec4( 10.0,  10.0, -10.0, 1.0), // 6
+        vec4( 10.0, -10.0, -10.0, 1.0)  // 7
     );
 
     return vertices;
@@ -167,6 +240,12 @@ function getColors() {
 
 
 function room() {
+    // TODO this is wrong!!
+    let scaler = scale(2, 2, 2);
+    modelViewMatrix = mult(scaler, modelViewMatrix);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
     for (let i = 0; i < 6; i++) {
         gl.drawArrays(gl.TRIANGLE_FAN, i * 4, 4);
     }
@@ -174,21 +253,19 @@ function room() {
 
 //----------------------------------------------------------------------------
 
+var cameraPosition = vec3(0, 0, 0);
+var cameraTarget = vec3(0, 0, 10);
+var cameraUp = vec3(0, 1, 0);
+
 function render() {
 
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-
-    const cameraPosition = vec3(0, 0, 0);
-    const cameraTarget = vec3(0, 0, 1);
-    const cameraUp = vec3(0, 1, 0);
 
     // modelViewMatrix = translate(0, 0, 30);
     // console.log(modelViewMatrix);
     // gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
     modelViewMatrix = lookAt(cameraPosition, cameraTarget, cameraUp);
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-    
     room();
 
     // modelViewMatrix = rotate(theta[Base], vec3(0, 1, 0 ));
