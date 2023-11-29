1934c6ddc712cacd69b1fa0794b74675154071ea
Mikecamdo <mcdoherty@smu.edu>
2023-11-29 12:17:37 -0600

    Preliminary Combination of Filters
    Changed the structure of the code so that a color filter and a geometric distortion filter can be used simultaneously

--- Program 3/CG_F23_Program3_DohertyMichael.js
+++ Program 3/CG_F23_Program3_DohertyMichael.js
@@ -4,6 +4,8 @@ var gl;	// Graphics context
 var program1, program2, program3, program4, 
 program5, program6, program7, program8, program9, program10,
 program11, program12; // programs for each of the fragment shaders
+var programs = [undefined];
+
 var videoFeed, videoTexture;
 
 var liveVideoFeed = true;
@@ -17,6 +19,8 @@ var vertices = new Float32Array ( [	// Use Javascript typed arrays for coordinat
 ]);
 
 var backgroundTexture;
+var distortionFilterTypeLocs = [undefined];
+var currentProgram = 1;
 
 // adapted from "Hello2DTexture_ImageFile.js" example
 function configureTexture(imgString) {
@@ -39,14 +43,15 @@ function configureTexture(imgString) {
     image.src = imgString;
 }
 
+var backgroundTexture;
 function configureBackgroundTexture(imgString) {
     let image = new Image();
-    let texture = gl.createTexture();
+    backgroundTexture = gl.createTexture();
 
     gl.activeTexture(gl.TEXTURE1);
 
     image.onload = function () {
-        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.bindTexture(gl.TEXTURE_2D, backgroundTexture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     
         gl.generateMipmap(gl.TEXTURE_2D);
@@ -79,7 +84,6 @@ function configureVideoTexture(video) {
 
 function updateTexture(texture, video) {
     gl.activeTexture(gl.TEXTURE0);
-
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
 
@@ -87,6 +91,9 @@ function updateTexture(texture, video) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
         gl.NEAREST_MIPMAP_LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, backgroundTexture);
 }
 
 function accessWebcam(video) {
@@ -120,20 +127,20 @@ window.onload = function init()
     //
     //  Load shaders and initialize attribute buffers
     //
-    program1 = initShaders(gl, "vertex-shader", "fragment-shader-1");
-    program2 = initShaders(gl, "vertex-shader", "fragment-shader-2");
-    program3 = initShaders(gl, "vertex-shader", "fragment-shader-3");
-    program4 = initShaders(gl, "vertex-shader", "fragment-shader-4");
-    program5 = initShaders(gl, "vertex-shader", "fragment-shader-5");
-    program6 = initShaders(gl, "vertex-shader", "fragment-shader-6");
-    program7 = initShaders(gl, "vertex-shader", "fragment-shader-7");
-    program8 = initShaders(gl, "vertex-shader", "fragment-shader-8");
-    program9 = initShaders(gl, "vertex-shader", "fragment-shader-9");
-    program10 = initShaders(gl, "vertex-shader", "fragment-shader-10");
-    program11 = initShaders(gl, "vertex-shader", "fragment-shader-11");
-    program12 = initShaders(gl, "vertex-shader", "fragment-shader-12");
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-1"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-2"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-3"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-4"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-5"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-6"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-7"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-8"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-9"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-10"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-11"));
+    programs.push(initShaders(gl, "vertex-shader", "fragment-shader-12"));
 
-    gl.useProgram(program1); // default to no filter
+    gl.useProgram(programs[currentProgram]); // default to no filter
 
     // texture array atrribute buffer
     var tBuffer = gl.createBuffer();
@@ -149,15 +156,15 @@ window.onload = function init()
         gl.STATIC_DRAW
     );
 
-    var texCoordLoc = gl.getAttribLocation(program1, "aTexCoord");
+    var texCoordLoc = gl.getAttribLocation(programs[1], "aTexCoord");
     gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(texCoordLoc);
 
-    texCoordLoc = gl.getAttribLocation(program2, "aTexCoord");
+    texCoordLoc = gl.getAttribLocation(programs[2], "aTexCoord");
     gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(texCoordLoc);
 
-    texCoordLoc = gl.getAttribLocation(program3, "aTexCoord");
+    texCoordLoc = gl.getAttribLocation(programs[3], "aTexCoord");
     gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(texCoordLoc);
 
@@ -166,68 +173,100 @@ window.onload = function init()
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-    var positionLoc = gl.getAttribLocation(program1, "aPosition");
+    var positionLoc = gl.getAttribLocation(programs[1], "aPosition");
     gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    positionLoc = gl.getAttribLocation(program2, "aPosition");
+    positionLoc = gl.getAttribLocation(programs[2], "aPosition");
     gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    positionLoc = gl.getAttribLocation(program3, "aPosition");
+    positionLoc = gl.getAttribLocation(programs[3], "aPosition");
     gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
 
-    gl.uniform1i(gl.getUniformLocation(program1, 'uTexMap'), 0);
-    
+    gl.uniform1i(gl.getUniformLocation(programs[1], 'uTexMap'), 0);
+    gl.uniform1i(gl.getUniformLocation(programs[1], 'uBackgroundTexture'), 1);
 
-    document.getElementById("imageFilter").onchange = function (event) {
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[1], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[2], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[3], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[4], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[5], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[6], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[7], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[8], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[9], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[10], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[11], 'distortionFilterType'));
+    distortionFilterTypeLocs.push(gl.getUniformLocation(programs[12], 'distortionFilterType'));
+
+    updateDistortionFilter(1);
+    gl.useProgram(programs[currentProgram]);
+
+    document.getElementById("colorFilter").onchange = function (event) {
         switch (event.target.value) {
             case '1': // no filter
-                gl.useProgram(program1);
-                break;
             case '2': // grayscale filter
-                gl.useProgram(program2);
-                break;
             case '3': // image negative filter
-                gl.useProgram(program3);
-                break;
             case '4': // saturated filter
-                gl.useProgram(program4);
-                break;
             case '5': // sepia filter
-                gl.useProgram(program5);
+            // case '6': // blurry filter
+            //     gl.useProgram(program6);
+            //     break;
+            // case '7': // distortion filter
+            //     gl.useProgram(program7);
+            //     break;
+            case '8': // constant threshold halftoning
+            case '9': // clustered dot screen
+            case '10': // bayer dot screen
+                currentProgram = event.target.value;
+
+                gl.useProgram(programs[currentProgram]);
+                
+                gl.uniform1i(gl.getUniformLocation(programs[currentProgram], 'uTexMap'), 0);
+                gl.uniform1i(gl.getUniformLocation(programs[currentProgram], 'uBackgroundTexture'), 1);
                 break;
-            case '6': // blurry filter
-                gl.useProgram(program6);
-                break;
-            case '7': // distortion filter
-                gl.useProgram(program7);
-                break;
-            case '8':
-                gl.useProgram(program8);
-                break;
-            case '9':
-                gl.useProgram(program9);
-                break;
-            case '10':
-                gl.useProgram(program10);
-                break;
-            case '11':
-                gl.useProgram(program11);
-                break;
-            case '12':
-                configureBackgroundTexture(gorillaBackground);
-                gl.useProgram(program12);
-                gl.uniform1i(gl.getUniformLocation(program12, 'uBackgroundTexture'), 1);
-                break;
-            case '13':
-                configureBackgroundTexture(christmasHatBackground);
-                gl.useProgram(program12);
-                gl.uniform1i(gl.getUniformLocation(program12, 'uBackgroundTexture'), 1);
-                break;
+            // case '11': // reflection
+            //     gl.useProgram(program11);
+            //     break;
+            // case '12': // gorilla
+            //     configureBackgroundTexture(gorillaBackground);
+            //     gl.useProgram(program12);
+            //     gl.uniform1i(gl.getUniformLocation(program12, 'uBackgroundTexture'), 1);
+            //     break;
+            // case '13': // christmas hat
+            //     configureBackgroundTexture(christmasHatBackground);
+            //     gl.useProgram(program12);
+            //     gl.uniform1i(gl.getUniformLocation(program12, 'uBackgroundTexture'), 1);
+            //     break;
             default:
                 gl.useProgram(program1);
+        }
+    }
+
+    document.getElementById("distortionFilter").onchange = function (event) {
+        switch (event.target.value) {
+            case '1': // none
+            case '2': // blurry
+            case '3': // distortion
+            case '4': // reflection
+                updateDistortionFilter(event.target.value);
+                gl.useProgram(programs[currentProgram]);
+                break;
+            case '5': // gorilla
+                configureBackgroundTexture(gorillaBackground);
+                updateDistortionFilter(event.target.value);
+                gl.useProgram(programs[currentProgram]);
+                break;
+            case '6': // christmas hat
+                configureBackgroundTexture(christmasHatBackground);
+                updateDistortionFilter(event.target.value);
+                gl.useProgram(programs[currentProgram]);
+                break;
+            default:
+                updateDistortionFilter(1);
+                gl.useProgram(programs[currentProgram]);
         }
     }
 
@@ -259,4 +298,42 @@ function render()
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4); // draws rectangle the size of the canvas for the texture to be mapped to
 
     requestAnimationFrame(render);	// Call to browser to refresh display
+}
+
+function updateDistortionFilter(distortionFilterType) {
+    gl.useProgram(programs[1]);
+    gl.uniform1i(distortionFilterTypeLocs[1], distortionFilterType);
+
+    gl.useProgram(programs[2]);
+    gl.uniform1i(distortionFilterTypeLocs[2], distortionFilterType);
+
+    gl.useProgram(programs[3]);
+    gl.uniform1i(distortionFilterTypeLocs[3], distortionFilterType);
+
+    gl.useProgram(programs[4]);
+    gl.uniform1i(distortionFilterTypeLocs[4], distortionFilterType);
+
+    gl.useProgram(programs[5]);
+    gl.uniform1i(distortionFilterTypeLocs[5], distortionFilterType);
+
+    gl.useProgram(programs[6]);
+    gl.uniform1i(distortionFilterTypeLocs[6], distortionFilterType);
+
+    gl.useProgram(programs[7]);
+    gl.uniform1i(distortionFilterTypeLocs[7], distortionFilterType);
+
+    gl.useProgram(programs[8]);
+    gl.uniform1i(distortionFilterTypeLocs[8], distortionFilterType);
+
+    gl.useProgram(programs[9]);
+    gl.uniform1i(distortionFilterTypeLocs[9], distortionFilterType);
+
+    gl.useProgram(programs[10]);
+    gl.uniform1i(distortionFilterTypeLocs[10], distortionFilterType);
+
+    gl.useProgram(programs[11]);
+    gl.uniform1i(distortionFilterTypeLocs[11], distortionFilterType);
+
+    gl.useProgram(programs[12]);
+    gl.uniform1i(distortionFilterTypeLocs[12], distortionFilterType);
 }
